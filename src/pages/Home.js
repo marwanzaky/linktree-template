56ad899b60b0e6d584863f345c454986635c78a8
Marwan Zaky <marouaneezzaky@gmail.com>
2022-09-02 04:16:13 +0100

    First release

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import Profile from "../sections/Profile";
+
+function Home() {
+    return (
+        <div className="Home">
+            <Profile />
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/sections/Profile.js
@@ -0,0 +1,48 @@
+const profile = {
+    fullname: 'Marwan Zaky',
+    bio: 'full-stack web dev 👨🏻‍💻',
+    picture: 'https://avatars.githubusercontent.com/u/64248203?v=4',
+    socialMedia: [
+        { name: 'Buy me a coffee ☕️', url: 'https://www.buymeacoffee.com/marwanzaky' },
+        { name: 'Visit my portfolio 🍳', url: 'marwanzaky.com' },
+        { name: 'Learn web dev 💻', url: '#' },
+        { name: 'Visit my github 🤓', url: 'https://github.com/marwanzaky' },
+        { name: 'Follow my twitter 💬', url: 'https://twitter.com/marwan_zaky_dev' }
+    ]
+}
+
+const colors = [
+    "#F4F1DE",
+    "#E07A5F",
+    "#3D405B",
+    "#81B29A",
+    "#F2CC8F"
+];
+
+function Button(props) {
+    const style = {
+        backgroundColor: colors[props.index]
+    }
+
+    return (
+        <a href={props.url} className="button" style={style}>
+            {props.name}
+        </a>
+    )
+}
+
+function Profile() {
+    return (
+        <section className="profile">
+            <div className="profile-picture"><img src={profile.picture}></img></div>
+            <div className="profile-fullname">{profile.fullname}</div>
+            <div className="profile-bio">{profile.bio}</div>
+
+            <div className="social_media flex flex-col w-full sm:w-[300px]">
+                {profile.socialMedia.map((el, i) => <Button name={el.name} url={el.url} index={i} />)}
+            </div>
+        </section>
+    );
+}
+
+export default Profile;
